784c7e2749410cb33c0b9b4532d5919493efa5a4
Abdullah Arshad <161123631+Abdullah-765@users.noreply.github.com>
2024-03-30 02:17:50 +0500

    Cli_todo_list

---
+++ project03_to_do_list/index.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let List = [];
+let condition = true;
+while (condition) {
+    let moreList = await inquirer.prompt([
+        {
+            name: "todos",
+            type: "input",
+            message: "Add your todos",
+        },
+        //Using list instead of confirm type
+        {
+            name: "addMore",
+            type: "list",
+            message: "Do you want to add more todos",
+            choices: ["yes", "no"],
+        },
+    ]);
+    moreList.addMore === "yes" ? (condition = true) : (condition = false);
+    List.push(moreList.todos);
+}
+// adding a bullet using forEach loop
+let i = 1;
+List.forEach((element, index) => {
+    console.log(`${index + 1}. ${element}`);
+});
